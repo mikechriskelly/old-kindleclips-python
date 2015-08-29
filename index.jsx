3a3337aabc347fb510370e6ace5957588a9db394
Mike Kelly <mikechriskelly@gmail.com>
2015-08-29 15:48:36 -0700

    Test material ui component

--- index.jsx
+++ index.jsx
@@ -2,40 +2,11 @@ import React from 'react';
 import mui from 'material-ui';
 require('./style.css');
 
-
-var ThemeManager = new mui.Styles.ThemeManager();
-var RaisedButton = mui.RaisedButton;
-
-var MyAwesomeReactComponent = React.createClass({
-
-  childContextTypes: {
-    muiTheme: React.PropTypes.object
-  },
-
-  getChildContext: function() {
-    return {
-      muiTheme: ThemeManager.getCurrentTheme()
-    };
-  },
-
-  render: function() {
-    return (
-        <RaisedButton label="Default" />
-    );
-  }
-
+loadJSON(function(response) {
+  var actual_JSON = JSON.parse(response);
+  console.log(actual_JSON);
+  React.render(<App clippings={actual_JSON} />, document.querySelector("#myApp"));
 });
-
-module.exports = MyAwesomeReactComponent;
-
-
-export class Header extends React.Component {
-  render() {
-    return (
-      <h1>I'm a Header</h1>
-    );
-  }
-}
 
 export class App extends React.Component {
   render() {
@@ -49,4 +20,44 @@ export class App extends React.Component {
   }
 }
 
-React.render(<App/>, document.querySelector("#myApp"));
+var ThemeManager = new mui.Styles.ThemeManager();
+var RaisedButton = mui.RaisedButton;
+
+var MyAwesomeReactComponent = React.createClass({
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+  getChildContext: function() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+  render: function() {
+    return (
+        <RaisedButton label="Default" />
+    );
+  }
+});
+module.exports = MyAwesomeReactComponent;
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <h1>I'm a Header</h1>
+    );
+  }
+}
+
+
+
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType('application/json');
+  xobj.open('GET', './data/my_clippings.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == '200') {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);  
+}
