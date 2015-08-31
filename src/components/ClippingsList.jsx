86b9d15220e17f83762cb22bcccf52bb960e66ff
Mike Kelly <mikechriskelly@gmail.com>
2015-08-30 22:28:05 -0700

    Filter on title too

--- src/components/ClippingsList.jsx
+++ src/components/ClippingsList.jsx
@@ -5,12 +5,14 @@ class ClippingsList extends React.Component {
   render() {
 
     var rows = [];
+    var filter = this.props.filterText.toUpperCase();
+
     this.props.clippings.forEach(function(clip) {
-      if(clip.text.indexOf(this.props.filterText) === -1) {
+      if(clip.text.toUpperCase().indexOf(filter) === -1 && clip.title.toUpperCase().indexOf(filter) === -1) {
         return;
       }
       rows.push(<Clip clip={clip}/>);
-    }.bind(this));
+    });
 
     return (
       <div>
