773989802cb25784b1fa5fa9e19c9d5eb0800a5e
Mike Kelly <mikechriskelly@gmail.com>
2015-10-24 15:00:03 -0700

    Add stylesheet

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import AppRoot from './components/AppRoot';
 import { Router, Route } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+
 import './style/skeleton.css';
+import './style/style.css';
 
 // Opt-out of persistent state and remove hash from url
 var history = createBrowserHistory({
