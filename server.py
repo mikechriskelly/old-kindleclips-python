4f0cbfd2bb7354e8bde512a43aef21b2ed2ba4aa
Mike Kelly <mikechriskelly@gmail.com>
2015-11-09 22:27:35 -0800

    Use port variable

--- server.py
+++ server.py
@@ -16,4 +16,7 @@ if __name__ == '__main__':
       'tools.staticdir.dir': BUILD_DIR
     }
   }
+
+  cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+  cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
   cherrypy.quickstart(KindleClippingsViewer(), '/', conf)
