88243bddcda490785e38c2683d0145a85c6ab630
Mike Kelly <mikechriskelly@gmail.com>
2015-12-13 17:33:32 -0800

    Allow client side routing with  hash

--- app/index.jsx
+++ app/index.jsx
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom';
 import AppRoot from './components/AppRoot';
 import Main from './components/Main';
 import { Router, Route, IndexRoute } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import './style/skeleton.css';
 import './style/style.css';
 
-// Opt-out of persistent state and remove hash from url
-let history = createBrowserHistory({
-  queryKey: false
-});
-
 ReactDOM.render((
-  <Router history={history}>
+  <Router>
     <Route path='/' component={AppRoot}>
       <IndexRoute component={Main} />
     </Route>
